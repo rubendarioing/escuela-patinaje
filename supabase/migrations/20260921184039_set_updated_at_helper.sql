aeec3f015e53ac928f3ddc60ada671f4e39e9f12
rubendarioing <ruben_dario_ing@hotmail.com>
2026-09-21 13:49:03 -0500

    db: agregar set_update_at, admin_profiles, is_staff e is_admin

---
+++ supabase/migrations/20260921184039_set_updated_at_helper.sql
@@ -0,0 +1,11 @@
+-- Trigger genérico para mantener updated_at
+create or replace function set_updated_at()
+returns trigger
+language plpgsql
+set search_path = public
+as $$
+begin
+  new.updated_at = now();
+  return new;
+end;
+$$;
